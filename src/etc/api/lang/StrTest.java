c66a1f1888c4598a50c40db012825bcffc3573b1
sinjawon <qlqhroal@naver.com>
2025-09-24 18:36:53 +0900

    스트링빌더 랑 스트링 시간차이

--- src/etc/api/lang/StrTest.java
+++ src/etc/api/lang/StrTest.java
@@ -26,6 +26,10 @@ public class StrTest {
         long result = Math.round(pi*1000);
         System.out.println("result = " + result*0.001);
 
+        //쓸 일이 많으면 오버라이딩 하면될것같은데
+        //내가 개쩔게 잘하면 만들어보자
+       long result2 =  Math.round(3.14);
+        System.out.println("result = " + result);
 
     }
 }
